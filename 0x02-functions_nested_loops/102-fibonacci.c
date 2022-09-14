efbdd8bc8ab36239aec52f8e28d5df0b833e1006
Jonzay7 <enijones007@gmail.com>
2022-09-14 09:13:05 -0700

    My commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,16 +13,16 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
-			sum = fib1 + fib2;
-			printf("%lu", sum);
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-			fib1 = fib2;
-			fib2 = sum;
+		fib1 = fib2;
+		fib2 = sum;
 
-			if (count == 49)
-				printf("\n");
-			else
-				printf(", ");
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 	return (0);
 }
