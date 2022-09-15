3b92e064b1ca278803b5a0220a347802a9c85c5d
Jonzay7 <enijones007@gmail.com>
2022-09-15 14:23:59 -0700

    My commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,82 +1,191 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
+
+
 /**
+
  * _putchar - puts character to standard output
- * @c: character to be put to standard output
+
+ * @c: character to put to standard output
+
  */
+
+
 
 int _putchar(char c);
 
+
+
 /**
- * _isupper - determines uppercase or not
- * @c: character to determine
- * Return: true or false
+
+ * _isupper - checks for uppercase character
+
+ * @c: character to be checked
+
+ * Return: 1 if true, 0 if false
+
  */
+
+
 
 int _isupper(int c);
 
+
+
 /**
- * _isdigit - checks if it is digit
- * @c: character to check
- * Return: returns boolean
+
+ * _isdigit - checks for a digit 0 through 9
+
+ * @c: digit to be checked
+
+ * Return: 1 if true, 0 if false
+
  */
+
+
 
 int _isdigit(int c);
 
+
+
 /**
- * mul - multiplies stuff
- * @a: integer to multiply
- * @b: integer to multiply
- * Return: gives result
+
+ * mul - multiplies two integers
+
+ * @a: one integer
+
+ * @b: second integer
+
+ * Return: product of a and b
+
  */
+
+
 
 int mul(int a, int b);
 
+
+
 /**
- * print_numbers - prints the numbers
+
+ * print_numbers - print 0 to 9
+
  */
+
+
 
 void print_numbers(void);
 
+
+
 /**
- * print_most_numbers - prints the numbers
+
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+
  */
+
+
 
 void print_most_numbers(void);
 
+
+
 /**
- * more_numbers - prints the numbers
+
+ * more_numbers - print 0 to 14 ten times
+
  */
+
+
 
 void more_numbers(void);
 
+
+
 /**
- * print_line - prints a line
- * @n: integer to print stuff
+
+ * print_line(int n) - print the character "_" n times to draw a line
+
+ * @n: number of characters to draw
+
  */
+
+
 
 void print_line(int n);
 
+
+
 /**
- * print_diagonal - prints diagonal 
- * @n: integer to see how many stupid lines
+
+ * print_diagonal - print character "\" n times
+
+ * @n: number of characters to draw
+
  */
+
+
 
 void print_diagonal(int n);
 
+
+
 /**
- * print_square - prints a square
- * @size: prints the size of whatever
+
+ * print_square - print a square of given size
+
+ * @size: size to draw
+
  */
+
+
 
 void print_square(int size);
 
+
+
 /**
- * print_triangle - prints triangle
- * @size: size of triangle
+
+ * print_triangle - print traingle with #'s with given size
+
+ * @size: size of triangle to draw
+
  */
 
+
+
 void print_triangle(int size);
+
+
+
+/**
+
+ * print_triangle - print traingle with #'s with given size
+
+ * @size: size of triangle to draw
+
+ */
+
+
+
+void print_triangle(int size);
+
+
+
+/**
+
+ * print_number = print an integer, without using long, arrays, or pointers
+
+ * @n: number to be printed
+
+ */
+
+
+
+void print_number(int n);
+
+
 
 #endif
