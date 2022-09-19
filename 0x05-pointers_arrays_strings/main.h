290fe6d2bc6da7449e01f9ccefa0155c29596cea
Jonzay7 <enijones007@gmail.com>
2022-09-19 15:53:48 -0700

    My commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,42 +1,17 @@
-<<<<<<< HEAD
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-int _putchar(char c);
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-=======
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
-/**
-* void prototypes()
-* char prototypes()
-* int prototypes()
-*/
 void reset_to_98(int *n);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void swap_int(int *a, int *b);
->>>>>>> 54a0759dd2f673013431294240e0523648d18135
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-<<<<<<< HEAD
-int _atoi(char *s);
-
-#endif
-=======
 int _putchar(char c);
 int _strlen(char *s);
 int _atoi(char *s);
-#endif/MAIN_H/
->>>>>>> 54a0759dd2f673013431294240e0523648d18135
+#endif
