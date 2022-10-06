3597b86d05da3cfee7b5a1571c2b60d9b54086a3
Jonzay7 <enijones007@gmail.com>
2022-10-06 13:03:31 -0700

    phoenix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,27 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
- 
-/** 
-* *array_range - creates an array of integers and stores numbers min to max, 
-* both inclusive in the array 
-* @min: smallest number to be stored 
-* @max: largest number to be stored 
-* Return: pointer to the newly created array on success, 
-* else NULL if min > max or if malloc fails 
-*/
-
+/**
+ * array_range - A function that creates an array of integers
+ * @min: minimum number to start
+ * @max: maximum number to start
+ * Return: pointer
+ */
 int *array_range(int min, int max)
-{	
-	int *boy;
-	int girl, num, index;
+{
+	int *enny;
+	int enitan, i;
 
-       	if (min > max)
-	       	return (0);
-	girl = (max - min) + 1;
-	boy = malloc(sizeof(int) * girl);
-	if (boy == 0)
-		return (0);
-	for (num = min, index = 0; num <= max; index++, num++)
-		*(boy + index) = num;
-	return (boy);
+	if (min > max)
+		return (NULL);
+
+	enitan = max - min;
+	enny = malloc((enitan + 1) * sizeof(int));
+	if (enny == NULL)
+		return (NULL);
+
+	for (i = 0; i <= enitan; i++)
+		enny[i] = min++;
+
+	return (enny);
 }
